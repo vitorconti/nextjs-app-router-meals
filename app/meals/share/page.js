51b9278e3579c98a6fb7f59eb27749d306540f64
vitorcontiz <vitorcontizgit@gmail.com>
2024-02-17 15:27:39 -0300

    feat: add visual feedback

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,10 +1,13 @@
+'use client'
+import { useFormState } from 'react-dom'
 import ImagePicker from '@/components/images/image-picker';
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/action';
 import MealFormButton from '@/components/meals/meal-form-button';
 
 export default function ShareMealPage() {
- 
+  const [state, formAction] = useFormState(shareMeal, { message: null })
+
   return (
     <>
       <header className={classes.header}>
@@ -14,7 +17,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -42,11 +45,12 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-            <ImagePicker label={'Your image'} name='image'/>
+          <ImagePicker label={'Your image'} name='image' />
           <p className={classes.actions}>
-            <MealFormButton/>
+            <MealFormButton />
           </p>
         </form>
+        {state.message && <p>{state.message}</p>}
       </main>
     </>
   );
