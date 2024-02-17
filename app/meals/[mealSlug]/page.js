02aaa188b623a940c42bada212216a2f59605666
vitorcontiz <vitorcontizgit@gmail.com>
2024-02-17 15:58:51 -0300

    feat: adding meta data

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -2,23 +2,35 @@ import Image from 'next/image'
 import classes from './page.module.css'
 import { getMeal } from '@/lib/meals'
 import { notFound } from 'next/navigation'
-export default function MealsDetailPage({params}){
+
+export async function generateMetadata({ params }) {
     const meal = getMeal(params.mealSlug)
-    if (!meal){
+    if (!meal) {
+        notFound()
+    }
+    return {
+        title: meal.title,
+        description: meal.summary
+    }
+}
+
+export default function MealsDetailPage({ params }) {
+    const meal = getMeal(params.mealSlug)
+    if (!meal) {
         notFound()
     }
     meal.instructions = meal.instructions.replace(/\n/g, '<br/>')
     return <>
         <header className={classes.header}>
             <div className={classes.image}>
-                <Image src={meal.image} alt={meal.title} fill/>
+                <Image src={meal.image} alt={meal.title} fill />
             </div>
             <div className={classes.headerText}>
                 <h1>
                     {meal.title}
                 </h1>
                 <p className={classes.creator}>
-                    by <a href={`mailto:${meal.creator_email}`}>{meal.creator }</a>
+                    by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
                 </p>
                 <p className={classes.summary}>
                     {meal.summary}
@@ -30,7 +42,7 @@ export default function MealsDetailPage({params}){
             <p className={classes.instructions} dangerouslySetInnerHTML={{
                 __html: meal.instructions
             }}>
-                
+
             </p>
         </main>
     </>
--- app/meals/page.js
+++ app/meals/page.js
@@ -4,19 +4,24 @@ import { getMeals } from "@/lib/meals";
 import { Suspense } from "react";
 import MealsLoadingPage from "./loading-out";
 
+export const metadata = {
+    title: 'All meals',
+    description: 'Delicious meals, shared by our vibrant community.',
+};
 
-async function Meals(){
+
+async function Meals() {
     const meals = await getMeals()
-    return <MealsGrid meals={meals}/>
+    return <MealsGrid meals={meals} />
 }
 
-export default async function MealsPage(){
+export default async function MealsPage() {
 
     return <>
-        <MealsHeader/>
+        <MealsHeader />
         <main>
-            <Suspense fallback={<MealsLoadingPage/>}>
-                <Meals/>
+            <Suspense fallback={<MealsLoadingPage />}>
+                <Meals />
             </Suspense>
         </main>
     </>
